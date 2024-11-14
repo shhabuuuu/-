d42e75db3fdaea245214a6aa34f772148e4014c6
shhabuuuu <karinasabanova485@gmail.com>
2024-11-14 16:14:39 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Name: Karina"
+print(name, type(name))
+age = 19
+print(age, type(age))
+new_age = 1
+print(age + new_age, type(new_age))
+Is_student = True
+print(Is_student, type(Is_student))
