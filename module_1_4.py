9f78567a2853570d007c628fbaf5a21bbe24fc4a
shhabuuuu <karinasabanova485@gmail.com>
2024-11-14 18:39:44 +0300

    Add files via upload
    Module 1 4

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Какое Ваше любимое блюдо? ")
+print(26)
+print(my_string .upper())
+print(my_string .lower())
+my_string = "Люблю крабовый салат"
+print(my_string .replace("Люблю крабовый салат", "Люблюкрабовыйсалат"))
+print(my_string[0])
+print(my_string [-1])
